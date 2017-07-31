5e571a6d1c74c640c3bbd46c2fe803f917e1562a
Craig Emery <github@cemery.org.uk>
2017-07-31 12:30:22 +0100

    Someone changed the uploaded files

--- bash/aa.bash
+++ bash/aa.bash
@@ -2,14 +2,14 @@
 
 function clip ()
 {
-    local -rA clip_numbers=([cow]=0 [chick]=1 [goat]=2 [smb_warning]=3 [pizza_time]=4 [interuder_detected]=5 [darkside2]=6 [mario_die]=7 [yay_16]=8)
+    local -rA clip_numbers=([cow]=0 [pig]=1 [goat]=2 [smb_warning]=3 [pizza_time]=4 [intruder_detected]=5 [chick]=6 [mario_die]=7 [yay_16]=8 [sacrebleu]=9)
     local -r num="${clip_numbers[$1]}"
     if [[ "${num}" ]] ; then
         curl 'http://10.20.30.17/axis-cgi/playclip.cgi?clip='"${num}"
     fi
 }
 
-complete -W "cow chick goat smb_warning pizza_time interuder_detected darkside2 mario_die yay_16" clip
+complete -W "cow pig goat smb_warning pizza_time intruder_detected chick mario_die yay_16 sacrebleu" clip
 
 function __aa ()
 {
